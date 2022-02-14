844cb29fcb170b3d300f483c94e0a032c70220ee
Fellipe Augusto <Fellipe246@hotmail.com>
2022-02-14 20:34:47 -0300

    ProjetoHero

--- Models/Hero.cs
+++ Models/Hero.cs
@@ -2,27 +2,29 @@ namespace Aula_Csharp.Models
 {
     public class Hero
     {
-        public Hero(string Name, int Level, string HeroType, string Stats)
+        public Hero(string Name, int Level, string HeroType, int Hp, int Mp, string Stats)
         {
             this.Name = Name;
             this.Level = Level;
             this.HeroType = HeroType;
+            this.Hp = Hp;
+            this.Mp = Mp;
             this.Stats = Stats;
-
 
         }
 
         public string Name;
         public int Level;
         public string HeroType;
-
         public string Stats;
+        public int Hp;
+        public int Mp;
 
 
 
         public override string ToString()
         {
-            return this.Name + " " + this.Level + " " + this.HeroType + "" + this.Stats;
+            return this.Name + " " + this.Level + " " + this.HeroType + " " +  this.Hp + " " + this.Mp + " " + this.Stats;
             
         }
         
--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace ProjetoHeroi
     {
         public static void Main(string[] args)
         {
-            Hero hero = new Hero("Lupus", 50, "Gunslinger", "SS");
-            Hero Ninja = new Hero("Lass", 49, "Shinobi", "S");
-            Hero Fighter = new Hero("Jin", 85, "Illuminated", "SSS");
-            Hero Swordsman = new Hero("sieghart", 68, "Avatar", "S+");
+            Hero hero = new Hero("Lupus", 50, "Gunslinger", 800, 1500, "SS");
+            Hero Ninja = new Hero("Lass", 49, "Shinobi", 900, 12000, "S");
+            Hero Fighter = new Hero("Jin", 85, "Illuminated",19000, 50000, "SSS");
+            Hero Swordsman = new Hero("sieghart", 68, "Avatar",1000, 6000, "S+");
 
             Console.WriteLine(hero);
             Console.WriteLine(Ninja);
